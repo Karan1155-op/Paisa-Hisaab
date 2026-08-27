50c5299319966fb6e068035ea35626cc8fb09f27
Karan1155-op <pspokestar@gmail.com>
2026-08-27 09:18:48 +0530

    refactor: update UI navigation animations and styling for subpages and buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paisa-hisaab-v20';
+const CACHE_NAME = 'paisa-hisaab-v22';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
