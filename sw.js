dc1244ecdf15236d74c233c6f4edc0fad225dd4c
Karan1155-op <pspokestar@gmail.com>
2026-08-27 10:01:33 +0530

    refactor: simplify browser navigation history management by replacing pushState/popState with a unified ensureNavHistory approach

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paisa-hisaab-v28';
+const CACHE_NAME = 'paisa-hisaab-v30';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
