a766c87993cf1920b6e62671248341dcde589073
Karan1155-op <pspokestar@gmail.com>
2026-08-27 09:31:56 +0530

    feat: update cache version and redesign transaction cards and backup action buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paisa-hisaab-v23';
+const CACHE_NAME = 'paisa-hisaab-v24';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
