f3816f433d0377ed97f5fdf2a6416f2be6253647
Karan1155-op <pspokestar@gmail.com>
2026-08-28 06:57:42 +0530

    style: update transaction card styling and increment service worker cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paisa-hisaab-v34';
+const CACHE_NAME = 'paisa-hisaab-v35';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
