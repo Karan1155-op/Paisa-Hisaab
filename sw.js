835edf0e48982a38c35f81e265a89e7821878f95
Karan1155-op <pspokestar@gmail.com>
2026-08-28 07:15:24 +0530

    feat: redesign transaction filters and PDF button UI, and increment cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paisa-hisaab-v39';
+const CACHE_NAME = 'paisa-hisaab-v40';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
