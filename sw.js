d19e7a86eedb21635d32eadadbac8c90cd9cf444
Karan1155-op <pspokestar@gmail.com>
2026-08-28 06:50:55 +0530

    feat: add date-grouped dividers to transaction lists and update service worker cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paisa-hisaab-v32';
+const CACHE_NAME = 'paisa-hisaab-v33';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
