d4a1823ba8cf8f6800b5b84d886df6f031f89360
Karan1155-op <pspokestar@gmail.com>
2026-08-29 08:13:44 +0530

    style: update color palette and refresh cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paisa-hisaab-v51';
+const CACHE_NAME = 'paisa-hisaab-v52';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
