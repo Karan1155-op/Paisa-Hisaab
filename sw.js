36b4a67b44802b57e284ee06418be956f8948544
Karan1155-op <pspokestar@gmail.com>
2026-09-01 07:23:47 +0530

    refactor: move clear button to settings tab, remove direct time change feature, and update service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paisa-hisaab-v57';
+const CACHE_NAME = 'paisa-hisaab-v58';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
