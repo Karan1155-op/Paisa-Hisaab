924bd2430b786e6b35e42672adeec8f6d0fe1ef6
Karan1155-op <pspokestar@gmail.com>
2026-09-01 09:08:11 +0530

    feat: implement custom calendar component and replace native date inputs with consistent UI pills

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paisa-hisaab-v64';
+const CACHE_NAME = 'paisa-hisaab-v65';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
