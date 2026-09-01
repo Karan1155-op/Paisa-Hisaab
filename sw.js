bec64fd2ed2ee910583a0cd05bafb5be81d9a6c9
Karan1155-op <pspokestar@gmail.com>
2026-09-01 07:07:55 +0530

    feat: disable text selection and context menus while preventing automatic translation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'paisa-hisaab-v54';
+const CACHE_NAME = 'paisa-hisaab-v55';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
